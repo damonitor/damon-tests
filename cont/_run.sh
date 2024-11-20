76cc2d94a355563d41d17bcdc0fee17151ead923
SeongJae Park <sj@kernel.org>
2024-11-20 09:04:04 -0800

    cont/_run: test 6.12.y kernels
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- cont/_run.sh
+++ cont/_run.sh
@@ -40,6 +40,9 @@ fi
 	--tree_to_track stable \
 		https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git \
 		linux-6.11.y \
+	--tree_to_track stable \
+		https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git \
+		linux-6.12.y \
 	--tree_to_track stable-rc \
 		https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc.git \
 		linux-5.15.y \
@@ -52,6 +55,9 @@ fi
 	--tree_to_track stable-rc \
 		https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc.git \
 		linux-6.11.y \
+	--tree_to_track stable-rc \
+		https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable-rc.git \
+		linux-6.12.y \
 	--tree_to_track linus \
 		https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git \
 		master \
