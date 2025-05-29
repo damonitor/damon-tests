b3acdcbf611ce9815348f9bf34bcf7a92bacd58d
Enze Li <lienze@kylinos.cn>
2025-05-29 22:08:05 +0800

    corr/install_deps: Add supplemental packages for minimal installations
    This commit supplements missing packages for minimal installations on
    Ubuntu systems.  Additionally, it simplifies the Python package
    detection logic.
    
    Tested on Ubuntu 22.04 and 24.04.
    
    Signed-off-by: Enze Li <lienze@kylinos.cn>

--- corr/install_deps.sh
+++ corr/install_deps.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if ! sudo apt install -y xz-utils lftp python &> /dev/null
+# Basic requirements
+sudo apt install -y xz-utils lftp
+
+if ! sudo apt install -y python &> /dev/null
 then
 	# python package is not in Ubuntu 22.04
-	sudo apt install -y xz-utils lftp python-is-python3
+	sudo apt install -y python-is-python3
 fi
+
+# Required for minimal installations in Ubuntu 22.04 and 24.04.
+sudo apt install -y make gcc flex bison bc
 
 # required from v6.11-rc1 for arm64 build
 sudo apt install -y libgmp-dev libmpc-dev
