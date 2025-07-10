9a8fffeebbf9b0a97c85a5a7bc9c2ca5983baeeb
Enze Li <lienze@kylinos.cn>
2025-07-10 15:23:32 +0800

    corr/install_deps: Add Red Hat-based distros support
    Currently, damon_tests only supports testing on Debian-based distros.
    This patch extends the support to Red Hat-based distros, enabling
    broader test coverage.
    
    Tested on x86-64 Fedora 42 and Ubuntu 24.04.
    
    Signed-off-by: Enze Li <lienze@kylinos.cn>

--- corr/install_deps.sh
+++ corr/install_deps.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-# Basic requirements
-sudo apt install -y xz-utils lftp
+# Debian-based distros
+if which apt &> /dev/null; then
+	# Basic requirements
+	sudo apt install -y xz-utils lftp
 
-if ! sudo apt install -y python &> /dev/null
-then
-	# python package is not in Ubuntu 22.04
-	sudo apt install -y python-is-python3
+	if ! sudo apt install -y python &> /dev/null
+	then
+		# python package is not in Ubuntu 22.04
+		sudo apt install -y python-is-python3
+	fi
+
+	# Required for minimal installations in Ubuntu 22.04 and 24.04.
+	sudo apt install -y make gcc flex bison bc
+
+	# required from v6.11-rc1 for arm64 build
+	sudo apt install -y libgmp-dev libmpc-dev
+
+# Red Hat-based distros
+elif which dnf &> /dev/null; then
+	sudo dnf install -y make gcc gcc-c++ flex bison lftp gmp-devel \
+	libmpc-devel
+
+else
+	echo '#################################################################'
+	echo '# This distribution is not officially maintained by damon_tests #'
+	echo '# project.  You have to install required packages manually.     #'
+	echo '#################################################################'
 fi
-
-# Required for minimal installations in Ubuntu 22.04 and 24.04.
-sudo apt install -y make gcc flex bison bc
-
-# required from v6.11-rc1 for arm64 build
-sudo apt install -y libgmp-dev libmpc-dev
